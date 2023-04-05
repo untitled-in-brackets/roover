d9b44328a4909478cae73756d01a4c664d8fe274
Ryan Sheehan <sheehan.w.ryan@gmail.com>
2023-04-05 19:53:30 -0400

    play

--- src/hooks/useRoover/useRoover.ts
+++ src/hooks/useRoover/useRoover.ts
@@ -163,6 +163,8 @@ const useRoover = ({
       });
       setAudio(newAudio);
       playerRef.current = newAudio;
+      newAudio.play();
+      service.send(EVENTS.PLAY);
     } else {
       if (ready || paused) {
         audio.play();
